be7f83bb59dbc5e85e9775c418c607e6cf79a7f1
urmalina <142245641+urmalina@users.noreply.github.com>
2025-02-17 14:19:12 +0300

    added class manual lifting

--- manual_lifting.py
+++ manual_lifting.py
@@ -2,11 +2,6 @@ class ManualLifting:
     PRICE_PER_FLOOR_PER_100KG = 300
 
     def __init__(self, cargo_weight: int, floor: int, lift_available: bool):
-        """
-        :param cargo_weight: Вес груза
-        :param floor: На какой этаж поднимаем (если 1 этаж, то это 0 подъема)
-        :param lift_available: Можно ли использовать лифт
-        """
         self.cargo_weight = cargo_weight
         self.floor = max(floor - 1, 0)  # Этажи считаются с 1, а плата — за поднятие
         self.lift_available = lift_available
